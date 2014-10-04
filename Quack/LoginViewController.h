6e00fa24e5bd107871a801387fd553cc67da52f8
Connie Qi <connieqi@umich.edu>
2014-10-04 17:16:11 -0400

    add fb login view

---
+++ Quack/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Quack
+//
+//  Created by Connie Qi on 10/4/14.
+//  Copyright (c) 2014 eecs441group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
