c99e7620c7f3c3ebbc8e0f0395c248d845c14c17
Chelsea Pugh <chelsea.pugh93@gmail.com>
2014-10-04 15:02:41 -0400

    adding inbox view

---
+++ Quack/InboxViewController.h
@@ -0,0 +1,13 @@
+//
+//  InboxViewController.h
+//  Quack
+//
+//  Created by Chelsea Pugh on 10/4/14.
+//  Copyright (c) 2014 eecs441group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InboxViewController : UIViewController
+
+@end
