96fea83cc1bc8a727b499c56cfb84dcb7d8a16b7
Connie Qi <connieqi@umich.edu>
2014-11-20 19:19:24 -0500

    fix merge conflict

--- Quack/SendToViewController.m
+++ Quack/SendToViewController.m
@@ -40,16 +40,14 @@ static NSString *kClickableHeaderIdentifier = @"ClickableHeader";
     [self.tableSectionTitles addObject:@"Friends"];
     [self.tableSectionTitles addObject:@"Groups"];
     
-<<<<<<< HEAD
     self.selectedIndices = [[NSMutableArray alloc] init];
-=======
+
     UINib *nib = [UINib nibWithNibName:@"ClickableHeader" bundle:nil];
     [self.tableView registerNib:nib forCellReuseIdentifier:@"ClickableHeader"];
     
     self.tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
->>>>>>> 9770e7df330db0dc4899a578cf22e6370a64f6ac
 }
 
 - (void) viewDidAppear:(BOOL)animated {
@@ -84,40 +82,10 @@ static NSString *kClickableHeaderIdentifier = @"ClickableHeader";
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-<<<<<<< HEAD
-    static NSString *MyIdentifier = @"MyReuseIdentifier";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:MyIdentifier];
-    if (cell == nil) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault  reuseIdentifier:MyIdentifier];
-    }
-    NSDictionary *friend = [self.friends objectAtIndex:indexPath.row];
-    cell.textLabel.text = friend[@"name"];
-    
-    if ([self.selectedUsers containsObject:friend]) {
-        cell.accessoryType = UITableViewCellAccessoryCheckmark;
-    } else {
-        cell.accessoryType = UITableViewCellAccessoryCheckmark;
-    }
-    
-    if (cell.accessoryType == UITableViewCellAccessoryCheckmark) {
-        NSLog(@"with checkmark");
-    }
-    
-    return cell;
-}
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
-    [cell setAccessoryType:UITableViewCellAccessoryCheckmark];
-    
-    [self.selectedIndices addObject:indexPath];
-
-=======
     ClickableHeader *cell = [tableView dequeueReusableCellWithIdentifier:kClickableHeaderIdentifier];
     cell.selectedBackgroundView.backgroundColor = [UIColor quackFoamColor];
     cell.arrowImageView.image = [UIImage imageNamed:@"checkmark.png"];
-    NSDictionary *friend = [self._friends objectAtIndex:indexPath.row];
+    NSDictionary *friend = [self.friends objectAtIndex:indexPath.row];
     cell.sectionLabel.text = friend[@"name"];
     return cell;
 }
@@ -126,31 +94,16 @@ static NSString *kClickableHeaderIdentifier = @"ClickableHeader";
     return 60.0f;
 }
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
     ClickableHeader *cell = (ClickableHeader *)[tableView cellForRowAtIndexPath:indexPath];
     cell.arrowImageView.image = [UIImage imageNamed:@"checkmark_green.png"];
-    NSLog(@"row selected %@", cell.textLabel.text);
     
-    NSDictionary *friend = [self._friends objectAtIndex:indexPath.row];
-    NSLog(@"for %@", friend[@"name"]);
-    [self._selectedUsers addObject:[[Question alloc] initWithDictionary:friend]];
-    
-    for (NSDictionary *selected in self._selectedUsers) {
-        NSLog(@"sending to %@", selected[@"name"]);
-    }
->>>>>>> 9770e7df330db0dc4899a578cf22e6370a64f6ac
+    [self.selectedIndices addObject:indexPath];
+
     
 //    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
-
-//-(void)tableView:(UITableView *)tableView didDeselectRowAtIndexPath:(NSIndexPath *)indexPath {
-//    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
-//    [cell setAccessoryType:UITableViewCellAccessoryNone];
-//    
-//    NSDictionary *friend = [self.friends objectAtIndex:indexPath.row];
-//    [self.selectedUsers removeObject:friend];
-////    [tableView reloadData];
-//}
 
 - (IBAction)sendPressed {
     [self saveQuestion];
